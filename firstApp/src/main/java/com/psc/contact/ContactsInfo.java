a50f36f1c5f06cc091be7877924bc8d085ed7e22
dillip <tech.dillip111@gmail.com>
2014-11-13 13:09:32 +0530

    Changed the comments

--- firstApp/src/main/java/com/psc/contact/ContactsInfo.java
+++ firstApp/src/main/java/com/psc/contact/ContactsInfo.java
@@ -3,7 +3,7 @@ package com.psc.contact;
 public class ContactsInfo {
 	
 	public static void main(String[] args) throws Exception {
-		System.out.println("Hello World");
+		System.out.println("Hello PSC!!! Entering Git World");
 	}
 
 }
