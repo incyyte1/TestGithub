f57c1f23997b964a9d5da96d6b46c3e7628d229f
dillip <tech.dillip111@gmail.com>
2014-11-13 12:53:45 +0530

    Added a new class Contacts

---
+++ firstApp/src/main/java/com/psc/contact/Contacts.java
@@ -0,0 +1,10 @@
+package com.psc.contact;
+
+public class Contacts {
+	
+	public static void main(String[] args) throws Exception {
+		System.out.println("Hello World");
+		System.out.println("Hello World2");
+	}
+
+}
